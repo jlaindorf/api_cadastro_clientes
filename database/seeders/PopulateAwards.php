c0ca48b2f31af32c406d44681f49c2c73ff461db
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-08 20:17:50 -0300

    Adiciona premios populando com seeds

---
+++ database/seeders/PopulateAwards.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+use Faker\Factory;
+use App\Models\Award;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PopulateAwards extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 10; $i++) { // Altere 10 para o número desejado de registros
+            Award::create([
+                'description' => $faker->name(),
+                'local' => $faker->text(),
+                'value' => $faker->randomFloat(2, 100, 1000),
+                'amount' => $faker->numberBetween(1, 5),
+                'date' => $faker->date,
+            ]);
+        }
+
+    }
+}
